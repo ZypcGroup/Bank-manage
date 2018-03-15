908bb69406497eef2282a59f42bf5f07fc300348
sunmingming <1027908281@qq.com>
2018-03-15 20:28:29 +0800

    创建新文件，使后台连接数据库

--- bank/order/admin.py
+++ bank/order/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from order import models
+admin.site.register(models.User)
+admin.site.register(models.List)
--- bank/order/models.py
+++ bank/order/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    uid = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=25)
+    password = models.CharField(max_length=25)
+    type = models.CharField(max_length=10)
+
+
+
+class List(models.Model):
+    lid = models.IntegerField(primary_key=True)
+    danger = models.CharField(max_length=255)
+    thing = models.CharField(max_length=255)
+    desc = models.CharField(max_length=255)
+    money = models.CharField(max_length=255)
+    level = models.CharField(max_length=255)
+    time = models.CharField(max_length=255)
+    people = models.ForeignKey(User)
+    num = models.CharField(max_length=255)
+
+
