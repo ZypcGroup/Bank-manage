2124afdda268576976ddba83e85a3ad2a4c5833a
sunmingming <1027908281@qq.com>
2018-03-15 21:14:19 +0800

    更新models.py实现主键的自增

--- bank/order/models.py
+++ bank/order/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    uid = models.IntegerField(primary_key=True)
+    uid = models.AutoField(primary_key=True)#方法AutoField用来设置自增字段
     name = models.CharField(max_length=25)
     password = models.CharField(max_length=25)
     type = models.CharField(max_length=10)
@@ -10,7 +10,7 @@ class User(models.Model):
 
 
 class List(models.Model):
-    lid = models.IntegerField(primary_key=True)
+    lid = models.AutoField(primary_key=True)
     danger = models.CharField(max_length=255)
     thing = models.CharField(max_length=255)
     desc = models.CharField(max_length=255)
