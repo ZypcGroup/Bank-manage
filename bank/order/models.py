69223e4a96290c8c999cca08a9709e91cc094327
sunmingming <1027908281@qq.com>
2018-03-16 22:07:15 +0800

    更改models.py如果存在外键将影响预期效果，所以删除外键，

--- bank/order/models.py
+++ bank/order/models.py
@@ -6,7 +6,9 @@ class User(models.Model):
     name = models.CharField(max_length=25)
     password = models.CharField(max_length=25)
     type = models.CharField(max_length=10)
-
+    def __str__(self):
+        return self.name
+    #self.后边的参数必须是char类型的
 
 
 class List(models.Model):
@@ -17,7 +19,9 @@ class List(models.Model):
     money = models.CharField(max_length=255)
     level = models.CharField(max_length=255)
     time = models.CharField(max_length=255)
-    people = models.ForeignKey(User)
+    #people = models.ForeignKey(User,null=True,on_delete=models.SET_NULL,blank=True)        不再设置主键
+    name = models.CharField(max_length=25)
     num = models.CharField(max_length=255)
-
+    def __str__(self):
+        return self.name
 
